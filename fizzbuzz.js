9e37f9e2799da7c43dbdaf8408854c4c5351ad53
kennymorgan1 <kenneth.ezeugwu@photizzo.com>
2019-10-14 13:17:46 +0100

    ssolution

---
+++ fizzbuzz.js
@@ -0,0 +1,17 @@
+function fizzbuzz() {
+  let result;
+  for(let i=1; i<=100; i++) {
+    if((i%3===0) && (i%5===0)) {
+      result = 'FizzBuzz';
+    } else if (i%3 === 0) {
+      result = 'Fizz'
+    } else if (i%5 === 0) {
+      result = 'Buzz'
+    } else {
+      result = i
+    }
+    console.log(result);  
+  }
+}
+
+fizzbuzz();
